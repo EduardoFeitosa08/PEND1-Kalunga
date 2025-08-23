ec07078c16743738546724ca19ad45e28b2a95c7
Eduardo Feitosa <eduardo.edubatista008@gmail.com>
2025-08-23 12:42:56 -0300

    Produtos Aparecendo e Java Script Finalizado

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 'use strict'
 
-import produtos from "./produtos_atualizados.json" with {type:'json'}
+import produtos from "./produtos_atualizados.json" with { type: "json"};
 
 function criarConteudo(produto){
-    const estante = document.getElementById('container')
+    const estante = document.getElementById('container-principal')
     const card = document.createElement('div')
     const preview = document.createElement('div')
     const img = document.createElement('img')
@@ -13,24 +13,30 @@ function criarConteudo(produto){
     const preco = document.createElement('p')
     const button_comprar = document.createElement('button')
 
-    img.src = ('./img/' + produto.imagem)
+    img.src = './img/' + produto.imagem
     nome.textContent = produto.nome
-    preco.textContent = produto.preco
+    nome.classList.add('produto-nome')
+    preco.textContent = ('R$ ' + produto.preco)
+    preco.classList.add('preco')
     button_comprar.textContent = "Comprar"
+    download.textContent = "DOWNLOAD"
 
     estante.appendChild(card)
-    card.appendChildren(preview, info_produto)
-    preview.appendChildren(img, download)
+    card.append(preview, info_produto)
+    preview.append(img, download)
+    preview.classList.add('preview')
 
     card.classList.add('card')
     img.classList.add('imagem')
     download.classList.add('download')
     info_produto.classList.add('info-produto')
     button_comprar.classList.add('button-comprar')
-    info_produto.appendChild(button_comprar)
+
 
     document.documentElement.style.setProperty('--nome', info_produto.appendChild(nome))
     document.documentElement.style.setProperty('--preco', info_produto.appendChild(preco))
+
+    info_produto.appendChild(button_comprar)
 
 }
 
